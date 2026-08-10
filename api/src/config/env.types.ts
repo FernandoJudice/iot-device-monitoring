ed2c290bf9d8e2e09a92a36915554ee615f4622a
Fernando Judice <fernandocesar95@gmail.com>
2026-08-09 22:59:25 -0300

    build(api): includes api into docker compose

--- api/src/config/env.types.ts
+++ api/src/config/env.types.ts
@@ -5,7 +5,7 @@ export const EnvSchema = z.object({
 	KAFKA_ALARM_TOPIC: z.string(),
 	MONGODB_URI: z.string(),
 	REDIS_URL: z.string(),
-	FRONTEND_URL: z.string(),
+	// FRONTEND_URL: z.string(),
 	PORT: z.coerce.number(),
 	ACCESS_TOKEN_SECRET: z.string().min(32),
 	ACCESS_TOKEN_DURATION_VALUE: z.coerce.number(),
